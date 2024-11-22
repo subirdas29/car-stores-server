f4ab9333ed8f0fa5184dd1077acb0fd37e3ea93d
subirdas29 <subirdas1045@gmail.com>
2024-11-22 10:20:04 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
+
+export default app;
