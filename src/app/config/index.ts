c8518e3c1c6ea5119ef5bf5328a49c1ff8541443
subirdas29 <subirdas1045@gmail.com>
2024-11-23 23:38:19 +0600

    added

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -4,6 +4,6 @@ import path from 'path';
 dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
 export default {
-  port: process.env.PORT || 5001,
+  port: process.env.PORT || 4000,
   database_url: process.env.DATABASE_URL,
 };
